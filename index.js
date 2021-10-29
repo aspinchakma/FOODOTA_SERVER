0ab8c4dde626ea046682bc52412d8752c38767a4
aspinchakma <85893728+aspinchakma@users.noreply.github.com>
2021-10-29 23:51:08 +0600

    get details by id

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 app.use(cors())
 app.use(express.json())
@@ -32,6 +34,14 @@ async function server() {
             const foods = await food.toArray();
             res.send(foods)
             console.log('foods length', foods.length)
+        })
+
+        app.get('/food/details/:id', async (req, res) => {
+            const foodId = req.params.id;
+            const query = { _id: ObjectId(foodId) };
+            const foodDetails = await foodCollection.findOne(query);
+            res.send(foodDetails)
+
         })
 
     }
